4fabc86950380074fd2ee7fe30586243a7c426d5
Agung  Perdananto <agung.perdananto@sirclo.com>
2022-06-06 08:41:39 +0700

    define fuzzy variables

---
+++ fuzzy_1.py
@@ -0,0 +1,50 @@
+def down(x, xmin, xmax):
+    return (xmax - x) / (xmax - xmin)
+
+def up(x, xmin, xmax):
+    return (x - xmin) / (xmax - xmin)
+
+class Permintaan():
+    minimum = 2100
+    maximum = 3500
+
+    def turun(self, x):
+        if x >= self.maximum:
+            return 0
+        elif x<= self.minimum:
+            return 1
+        else:
+            return down(x, self.minimum, self.maximum)
+
+    def naik(self, x):
+        if x >= self.maximum:
+            return 1
+        elif x<= self.minimum:
+            return 0
+        else:
+            return up(x, self.minimum, self.maximum)
+
+class Persediaan():
+    minimum = 100
+    maximum = 250
+
+    def sedikit(self, x):
+        if x >= self.maximum:
+            return 0
+        elif x<= self.minimum:
+            return 1
+        else:
+            return down(x, self.minimum, self.maximum)
+
+    def banyak(self, x):
+        if x >= self.maximum:
+            return 1
+        elif x<= self.minimum:
+            return 0
+        else:
+            return up(x, self.minimum, self.maximum)
+
+
+class Produksi():
+    minimum = 1000
+    maximum = 5000
